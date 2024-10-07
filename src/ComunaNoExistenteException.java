e6b7bc5994988d575111024aadb36a18afdc2ff2
Vicente <vicenteorellana2011@gmail.com>
2024-10-06 21:37:46 -0300

    Tercer commit: se agregaron las clases que extienden de excepción

---
+++ src/ComunaNoExistenteException.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vicen
+ */
+public class ComunaNoExistenteException extends Exception {
+    public ComunaNoExistenteException(String message) {
+        super(message); 
+    }
+}
---
+++ src/RegionNoExistenteException.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vicen
+ */
+public class RegionNoExistenteException extends Exception {
+    public RegionNoExistenteException(String message) {
+        super(message); // Llama al constructor de la clase padre Exception
+    }
+}
